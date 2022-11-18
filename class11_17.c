8cab6a29ec7a8317d6c13751ac12e26f047843b7
xueQianBanQ <115210866+xueQianBanQ@users.noreply.github.com>
2022-11-18 23:40:07 +0800

    Add files via upload

--- class11_17.c
+++ class11_17.c
@@ -2,219 +2,239 @@
 //#include <stdio.h>
 //int main()
 //{
-//	char str1[] = "abc";
-//	char str2[] = "abc";
-//	char* p1 = "abc";
-//	char* p2 = "abc";
+//	long int price;
+//	int bonus;
+//	scanf("%ld", &price);
+//	if (price <= 1e5)
+//	{
+//		bonus = price * 0.1;
+//	}
+//	else if (price <= 2e5)
+//	{
+//		bonus = 1e4 + (price - 1e5) * 0.075;
+//	}
+//	else if (price <= 4e5)
+//	{
+//		bonus = 1e4 + 7500 + (price - 2e5) * 0.05;
+//	}
+//	else if (price <= 6e5)
+//	{
+//		bonus = 1e4 + 7500 + 1e4 + (price - 4e5) * 0.03;
+//	}
+//	else if (price <= 1e6)
+//	{
+//		bonus = 1e4 + 7500 + 1e4 + 6000 + (price - 6e5) * 0.015;
+//	}
+//	else
+//	{
+//		bonus = 1e4 + 7500 + 1e4 + 6000 + 6000 + (price - 1e6) * 0.01;
+//	}
+//	printf("bonus=%ld\n", bonus);
 //	return 0;
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//	char c;
+//    int i, j, sum = 0, n = 0;
+//    scanf("%d", &n);
+//    for (i = 1, j = n; i <= j; i++, j--)
+//    {
+//        if (n % 2 == 0 && i < j)
+//        {
+//            sum = sum + i + j;
+//        }
+//        else if (n % 2 == 1 && i < j)
+//        {
+//            sum = sum + i + j;
+//            if (i + 2 == j)
+//            {
+//                sum += (n + 1) / 2;
+//            }
+//        }
+//    }
+//    printf("sum = %d", sum);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int sum = 0;
+//	int m = 1;
+//	while (m)
+//	{
+//		printf("Input m:\n");
+//		scanf("%d", &m);
+//		if (m == 0)
+//		{
+//			break;
+//		}
+//		sum += m;
+//		printf("sum = %d\n", sum);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int x, y, z;
+//    for (x = 0; x <= 20; x++)
+//    {
+//        for (y = 0; y <= 33; y++)
+//        {
+//            z = 100 - x - y;
+//            if (5 * x + 3 * y + z / 3.0 == 100.0)
+//            {
+//                printf("x=%d, y=%d, z=%d\n", x, y, z);
+//            }
+//        }
+//    }
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//    int n, i, count = 2;
+//    printf("Input n:\n");
+//    scanf("%d", &n);
+//    for (i = 2; i <= sqrt(n); i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            printf("No!\n");
+//            break;
+//        }
+//        count++;
+//    }
+//    if (count == i && i != 2 || n == 3)
+//    {
+//        printf("Yes!\n");
+//    }
+//    else if (n <= 2)
+//    {
+//        printf("No!\n");
+//    }
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int x1, x2, a;
+//    do {
+//        printf("Input x1, x2:\n");
+//        a = scanf("%d,%d", &x1, &x2);
+//		while (getchar() != '\n');
+//    } while (x1 * x2 >= 0 || a != 2);
+//    printf("x1=%d,x2=%d\n", x1, x2);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x1, x2, a;
+//	do {
+//		printf("Input x1, x2:\n");
+//		a = scanf("%d,%d", &x1, &x2);
+//		if (x1 * x2 < 0 && a == 2)break;
+//		while (getchar() != '\n');
+//	} while (a < 2 || x1 * x2 >= 0);
+//	printf("x1=%d,x2=%d\n", x1, x2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int x = 1, days;
 //	int a = 0;
-//	while ((c = getchar()) != EOF) 
+//	do
 //	{
-//		putchar(c);
-//	}
-//	printf("sad");
-//	scanf("%d", &a);
-//
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//	char str1[30] = "abfsdg";
-//	char str2[30] = "abcfs";
-//	char str3[30] = "edsf";
-//	char str[30];
+//		printf("Input days:\n");
+//		a = scanf("%d", &days);
+//		getchar();
+//		if (a == 1 && days > 0)break;
+//	} while (a != 1 || days <= 0);
 //	int i = 0;
-//	for (i = 0; str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0'; i++)
+//	for (i = 1; i < days; i++)//循环days - 1次
 //	{
-//		;
+//		x = (x + 1) * 2;
 //	}
-//	printf("%d\n", str1[i] - str2[i]);
-//	printf("%d\n", strcmp(str1, str2));
-//	return 0;
-//}
-
-////从小到大输出单词
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//	char str1[30] = "abfsdg";
-//	char str2[30] = "abcfs";
-//	char str3[30] = "edsf";
-//	char str[30];
-//	if (strcmp(str1, str2) > 0)
-//	{
-//		strcpy(str, str1);
-//		strcpy(str1, str2);
-//		strcpy(str2, str);
-//	}
-//	if (strcmp(str2, str3) > 0)
-//	{
-//		strcpy(str, str2);
-//		strcpy(str2, str3);
-//		strcpy(str3, str);
-//	}
-//	if (strcmp(str1, str2) > 0)
-//	{
-//		strcpy(str, str1);
-//		strcpy(str1, str2);
-//		strcpy(str2, str);
-//	}
-//	printf("%s %s %s\n", str1, str2, str3);
-//	return 0;
-//}
-
-
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//	char a[6] = "abcd";         //报错
-//	char b[] = "abc";
-//	printf("%s\n", strcpy(b, a));
-//
+//	printf("x=%d\n", x);
 //	return 0;
 //}
 
 //#include <stdio.h>
-//#include <string.h>
 //int main()
 //{
-//	char words[6][20] = { "good","better","best","bad","worse","worst" };
-//	char temp[20];
-//	int i = 0;
-//	int j = 0;
-//
-//	for (i = 0; i < 6; i++)
+//	int i = 1;
+//	char password;
+//	printf("Input your password:\n");
+//	for (i = 1; i <= 6; i++)
 //	{
-//		printf("%s ", words[i]);
-//	}
-//	printf("\n");
-//	for (i = 0; i < 5; i++)
-//	{
-//		for (j = 0; j < 5 - i; j++)
+//		scanf("%c", &password);
+//		while (getchar() != '\n');
+//		if (password >= '0' && password <= '9')
 //		{
-//			if (strcmp(words[j], words[j + 1]) > 0)
-//			{
-//				strcpy(temp, words[j]);
-//				strcpy(words[j], words[j + 1]);
-//				strcpy(words[j + 1], temp);
-//			}
+//			printf("%c, you have enter %d-bits number\n", password, i);
+//		}
+//		else
+//		{
+//			printf("error\n");
+//			i--;
 //		}
 //	}
-//	for (i = 0; i < 6; i++)
-//	{
-//		printf("%s ", words[i]);
-//	}
-//	printf("\n");
 //	return 0;
 //}
+
 
 //#include <stdio.h>
 //int main()
 //{
-//	char s[50];
-//	int i = gets(s);
-//	printf("%p\n", i);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//void swap(int* a, int* b)
-//{
-//	int t;
-//	t = *a;
-//	*a = *b;
-//	*b = t;
-//}
-//int main()
-//{
-//	int a = 1;
-//	int b = 2;
-//	printf("a = %d b = %d\n", a, b);
-//	swap(&a, &b);
-//	printf("a = %d b = %d\n", a, b);
-//	return 0;
-//}
-
-////家庭作业
-//#include <stdio.h>
-//#include <stdlib.h>
-//int main()
-//{
-//	int a = 0;
-//	char b[50] = {'a'};//存放每个数字
-//	int i = 0;
-//	int count = 0;//查看位数
-//	int c0 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0, c9 = 0;
-//	printf("Please enter the number:\n");
-//	scanf("%d", &a);
-//	int temp = a;
-//	a = abs(a);
-//	while (a > 0)
+//	int i = 1;
+//	char password;
+//	printf("Input your password:\n");
+//	for (i = 1; i <= 6; i++)
 //	{
-//		b[i] = a % 10;
-//		a = a / 10;
-//		i++;
-//		count++;
-//	}
-//	printf("%d: %d bits\n", temp, count);
-//	i--;//使i = 4；
-//	while (i + 1)
-//	{
-//		switch (b[i])
+//		scanf("%c", &password);
+//		getchar();
+//		if (password >= '0' && password <= '9')
 //		{
-//		case 0:c0++;break;
-//		case 1:c1++;break;
-//		case 2:c2++;break;
-//		case 3:c3++;break;
-//		case 4:c4++;break;
-//		case 5:c5++;break;
-//		case 6:c6++;break;
-//		case 7:c7++;break;
-//		case 8:c8++;break;
-//		case 9:c9++;break;
+//			printf("%c, you have enter %d-bits number\n", password, i);
 //		}
-//		i--;
-//	}
-//	int c[10] = { c0, c1, c2, c3, c4, c5, c6, c7, c8, c9 };
-//	for (i = 0; i < 10; i++)
-//	{
-//		if (c[i])
+//		else
 //		{
-//			switch (i)
-//			{
-//			case 0:printf("0: %d\n", c0); break;
-//			case 1:printf("1: %d\n", c1); break;
-//			case 2:printf("2: %d\n", c2); break;
-//			case 3:printf("3: %d\n", c3); break;
-//			case 4:printf("4: %d\n", c4); break;
-//			case 5:printf("5: %d\n", c5); break;
-//			case 6:printf("6: %d\n", c6); break;
-//			case 7:printf("7: %d\n", c7); break;
-//			case 8:printf("8: %d\n", c8); break;
-//			case 9:printf("9: %d\n", c9); break;
-//			}
+//			printf("error\n");
+//			i--;
 //		}
+//		getchar();
 //	}
-//	
 //	return 0;
 //}
+
 
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-	double price;
-	int bonus;
-	scanf("%ld")
+	int a = 0;
+	int i = 0;
+	int count = 0;//查看位数
+	printf("Please enter the number:\n");
+	scanf("%d", &a);
+	int temp = a;
+	a = abs(a);
+	while (a > 0)
+	{
+		a = a / 10;
+		i++;
+		count++;
+	}
+	printf("%d: %d bits\n", temp, count);
 	return 0;
 }
